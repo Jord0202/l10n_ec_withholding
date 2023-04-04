4e5ca2a9b91e6bdced58dc90399b67a6b0ddc37f
gomezgleonardob <gomezgleonardob@riseup.net>
2023-04-04 12:16:02 -0500

    [IMP]:Minor_Changes_Model_Wizard_Withholding

--- l10n_ec_withhold/__manifest__.py
+++ l10n_ec_withhold/__manifest__.py
@@ -12,18 +12,13 @@
         "python": ["cryptography==36.0.0", "xmlsig==0.1.9", "xades==0.2.4", "zeep"]
     },
     "data": [
-        #"security/ir.model.access.csv",
-        #"data/account_chart_template_data.xml",
+        "security/ir.model.access.csv",
         "data/account_tax_group_data.xml",
-        "data/l10n_latam.document.type.csv",
-        #"data/account_tax_report_data.xml",
-        #"data/account_tax_template_withhold_profit_data.xml",
-        #"data/account_tax_template_withhold_vat_data.xml",
         "views/res_partner.xml",
         "views/account_move_view.xml",
         "views/account_tax_view.xml",
         "views/wizard_withhold.xml",
-        
+        "data/l10n_latam.document.type.csv",
     ],
     "installable": True,
     "auto_install": False,
